8699e6349518fb79aa632c62d00a6ec94a8ad069
benwang2 <eb.benwang@gmail.com>
2022-01-18 18:32:47 -0500

    Implemented Retrofit for GitHub API

---
+++ app/src/main/java/com/example/githubclient/GithubAPI.kt
@@ -0,0 +1,57 @@
+package com.example.githubclient
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+data class Repository (
+    @SerializedName("name") val name:                   String,
+    @SerializedName("watchers_count") val watching:     Int,
+    @SerializedName("forks_count") val forked:          Int,
+    @SerializedName("stargazers_count") val starred:    Int
+)
+
+data class User (
+    @SerializedName("login") val username:          String,
+    @SerializedName("name") val displayName:        String,
+
+    @SerializedName("followers") val numFollowers:  Int,
+    @SerializedName("following") val numFollowing:  Int,
+    @SerializedName("public_repos") val numRepos:   Int,
+
+    @SerializedName("followers_url") val followers: String,
+    @SerializedName("following_url") val following: String,
+    @SerializedName("repos_url") val repos:         String
+)
+
+interface GithubAPI {
+
+    @GET("users/{username}")
+    fun getUser(@Path("username") username: String): Call<User>
+
+    @GET("users/{username}/followers")
+    fun getFollowers(@Path("username") username: String): Call<List<User>>
+
+    @GET("users/{username}/following")
+    fun getFollowing(@Path("username") username: String): Call<List<User>>
+
+    @GET("users/{username}/repos")
+    fun getRepositories(@Path("username") username: String): Call<List<Repository>>
+
+    companion object {
+        val BASE_URL = "https://api.github.com/"
+
+        fun create() : GithubAPI {
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(GithubAPI::class.java)
+
+        }
+    }
+}
--- app/src/main/java/com/example/githubclient/MainActivity.kt
+++ app/src/main/java/com/example/githubclient/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.githubclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private val githubAPI = GithubAPI.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val submitUsername: Button = findViewById(R.id.submitUsername)
+
+        submitUsername.setOnClickListener {
+            val username: String = findViewById<EditText>(R.id.githubUsername).text.toString()
+            val call = githubAPI.getFollowers(username)
+            call.enqueue(object : Callback<List<User>> {
+                override fun onFailure(call: Call<List<User>>, t: Throwable) {}
+
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                    TODO("NOT IMPLEMENTED")
+                }
+
+            })
+        }
+
     }
 }

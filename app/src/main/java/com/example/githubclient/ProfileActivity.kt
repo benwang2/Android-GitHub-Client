1946b83b5dd325872848e704800273bf81f461a3
benwang2 <eb.benwang@gmail.com>
2022-01-25 00:54:38 -0500

    Profile activity now displays followers of user.

--- app/src/main/java/com/example/githubclient/ProfileActivity.kt
+++ app/src/main/java/com/example/githubclient/ProfileActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TableRow
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
@@ -19,6 +21,9 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val recyclerView: RecyclerView = this.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         val displayName: TextView = this.findViewById(R.id.displayName)
         val githubUsername: TextView = this.findViewById(R.id.githubUsername)
@@ -49,10 +54,10 @@ class ProfileActivity : AppCompatActivity() {
         numFollowing.text = intent.getStringExtra("numFollowing")
         numRepos.text = intent.getStringExtra("numRepos")
 
-//        val followers = githubAPI.getFollowers(intent.getStringExtra("githubUsername")!!)
-//        val following = githubAPI.getFollowing(intent.getStringExtra("githubUsername")!!)
+        val followers = githubAPI.getFollowers(intent.getStringExtra("githubUsername")!!)
+        val following = githubAPI.getFollowing(intent.getStringExtra("githubUsername")!!)
         val starred = githubAPI.getStarred(intent.getStringExtra("githubUsername")!!)
-//        val repositories = githubAPI.getRepositories(intent.getStringExtra("githubUsername")!!)
+        val repositories = githubAPI.getRepositories(intent.getStringExtra("githubUsername")!!)
 
         starred.enqueue(object : Callback<List<Repository>> {
             override fun onFailure(call: Call<List<Repository>>, t: Throwable) = TODO("Not yet implemented")
@@ -60,6 +65,16 @@ class ProfileActivity : AppCompatActivity() {
                 call: Call<List<Repository>>,
                 response: Response<List<Repository>>
             ) = numStarred.setText(response.body()!!.size.toString() + (if (response.body()!!.size == 100) "+" else ""))
+        })
+
+        followers.enqueue(object: Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                val adapter = ProfileRecyclerAdapter(response.body()!!)
+                recyclerView.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<User>>, t: Throwable) = TODO("Not yet implemented")
+
         })
 
     }
--- app/src/main/java/com/example/githubclient/ProfileRecyclerAdapter.kt
+++ app/src/main/java/com/example/githubclient/ProfileRecyclerAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class ProfileRecyclerAdapter(private val profiles: List<User>) : RecyclerView.Adapter<ProfileRecyclerAdapter.ViewHolder>() {
 
@@ -33,15 +34,21 @@ class ProfileRecyclerAdapter(private val profiles: List<User>) : RecyclerView.Ad
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.activity_profile, parent, false)
+        val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.profile_card_layout, parent, false)
 
         return ViewHolder(layoutView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val profile: User = profiles[position]
+
         holder.displayName.text = profile.displayName
         holder.githubUsername.text = profile.username
+        Picasso.get().load(profile.avatarUrl).into(holder.profilePicture)
+        holder.followers.text = profile.numFollowers.toString()
+        holder.followers.text = profile.numFollowing.toString()
+        holder.repositories.text = profile.numRepos.toString()
+
     }
 
     override fun getItemCount(): Int = profiles.size

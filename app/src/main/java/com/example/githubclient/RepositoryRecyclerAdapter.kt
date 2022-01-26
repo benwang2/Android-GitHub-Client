c95331406acaf0714764d541f0fdb05d18800e11
benwang2 <eb.benwang@gmail.com>
2022-01-26 11:29:37 -0500

    Added icons, created recycler adapter and layout for repositories.

---
+++ app/src/main/java/com/example/githubclient/RepositoryRecyclerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.githubclient
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RepositoryRecyclerAdapter(private val repos: List<Repository>) : RecyclerView.Adapter<RepositoryRecyclerAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
